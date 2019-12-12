8687871931e8f57cfdd754bc07f4791393aee114
Sam O-A <yibaah@gmail.com>
2019-12-12 08:47:13 -0500

    plus+ 3

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -1,28 +1,39 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 };
 const reducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
-    return {
-      counter: state.counter + 1
-    };
+    const newState = Object.assign({}, state);
+    newState.counter = state.counter + 1;
+    return newState;
   }
 
   if (action.type === "DECREMENT") {
     return {
-      counter: state.counter - 1
+      ...state,
+      counter: state.counter + action.val
     };
   }
 
   if (action.type === "ADD") {
     return {
+      ...state,
       counter: state.counter + action.val
     };
   }
 
   if (action.type === "SUBSTRACT") {
     return {
+      ...state,
       counter: state.counter - action.val
+    };
+  }
+
+  if (action.type === "STORE_RESULT") {
+    return {
+      ...state,
+      results: state.results.concat({ id: new Date(), value: state.counter })
     };
   }
   return state;
--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -6,7 +6,8 @@ import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
   state = {
-    counter: 0
+    counter: 0,
+    results: []
   };
 
   counterChangedHandler = (action, value) => {
@@ -53,6 +54,17 @@ class Counter extends Component {
           label="Subtract 5"
           clicked={this.props.onSubtractCounter}
         />
+        <hr />
+        <button onClick={this.props.onStoreResult}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map(strResult => {
+            return (
+              <li key={strResult.id} onClick={this.props.onDeleteResult}>
+                {strResult.value}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
@@ -60,7 +72,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr: state.counter
+    ctr: state.counter,
+    storedResults: state.results
   };
 };
 
@@ -69,7 +82,9 @@ const mapDispatchToProps = dispatch => {
     onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
     onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
     onAddCounter: () => dispatch({ type: "ADD", val: 10 }),
-    onSubtractCounter: () => dispatch({ type: "SUBSTRACT", val: 15 })
+    onSubtractCounter: () => dispatch({ type: "SUBSTRACT", val: 15 }),
+    onStoreResult: () => dispatch({ type: "STORE_RESULT" }),
+    onDeleteResult: () => dispatch({ type: "DELETE_RESULT" })
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
